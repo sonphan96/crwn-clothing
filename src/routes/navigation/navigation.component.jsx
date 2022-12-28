00aeb37a3b68105596fe9e6ff3a87e3301059f42
Johnson Phan <phantruongson96@gmail.com>
2022-12-28 00:51:52 -0500

    add sign in to navigation component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -17,6 +17,10 @@ const Navigation = () => {
           <Link className='nav-link' to='/shop'>
             Shop
           </Link>
+
+          <Link className='nav-link' to='/sign-in'>
+            SIGN IN
+          </Link>
         </div>
       </div>
       <Outlet />
