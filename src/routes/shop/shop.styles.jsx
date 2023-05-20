0f72f05b2750e8a419284bb188d5195c0eba5ae2
Johnson Phan <phantruongson96@gmail.com>
2023-05-20 01:38:17 -0400

    add redirects file for netlify

---
+++ src/routes/shop/shop.styles.jsx
@@ -0,0 +1,10 @@
+// import styled from 'styled-components';
+
+// export const ProductsContainer = styled.
+
+// .products-container {
+//   display: grid;
+//   grid-template-columns: repeat(4, 1fr);
+//   column-gap: 10px;
+//   row-gap: 50px;
+// }
